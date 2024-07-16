89b8c7932e55d5d99466c8cefd82a45b5b8bc278
MunrraMT <dev.armsantos@gmail.com>
2024-07-15 22:11:45 -0400

    Understanding closures

--- cap3/web_app/src/main.rs
+++ cap3/web_app/src/main.rs
@@ -5,11 +5,14 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     HttpServer::new(|| {
+        println!("http server factory is firing");
+
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
             .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
     })
+    .workers(3)
     .bind("0.0.0.0:8080")?
     .run()
     .await
