94838ab275b9850f4d023a3ac1f9ee7aa5b7b480
MunrraMT <dev.armsantos@gmail.com>
2024-07-15 23:35:31 -0400

    Exploring async and await with web programming

--- cap3/web_app/src/main.rs
+++ cap3/web_app/src/main.rs
@@ -1,27 +1,23 @@
-use std::{thread, time};
+use std::io;
 
-fn main() {
-    let now = time::Instant::now();
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-    let thread_one = thread::spawn(|| do_something(1));
-    let thread_two = thread::spawn(|| do_something(2));
-    let thread_three = thread::spawn(|| do_something(3));
-
-    let result_one = thread_one.join();
-    let result_two = thread_two.join();
-    let result_three = thread_three.join();
-
-    println!("time elapsed {:?}", now.elapsed());
-    println!(
-        "result {}",
-        result_one.unwrap() + result_two.unwrap() + result_three.unwrap()
-    );
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("0.0.0.0:8080")?
+    .bind("0.0.0.0:8081")?
+    .workers(3)
+    .run()
+    .await
 }
 
-fn do_something(number: i8) -> i8 {
-    println!("number {} is running", number);
-    let two_seconds = time::Duration::new(2, 0);
-    thread::sleep(two_seconds);
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
 
-    2
+    format!("hello {}!", name)
 }
