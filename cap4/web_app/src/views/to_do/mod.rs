85f9b3590170cf717b52e259e67b3bcfc0f2bfd6
MunrraMT <dev.armsantos@gmail.com>
2024-07-17 08:34:08 -0400

    Using macros for JSON serialization

--- cap4/web_app/src/views/to_do/mod.rs
+++ cap4/web_app/src/views/to_do/mod.rs
@@ -1,7 +1,12 @@
-use actix_web::web::{post, scope, ServiceConfig};
+use actix_web::web::{get, post, scope, ServiceConfig};
 
 mod create;
+mod get;
 
 pub fn to_do_views_factory(app: &mut ServiceConfig) {
-    app.service(scope("v1/item").route("create/{title}", post().to(create::create)));
+    app.service(
+        scope("v1/item")
+            .route("create/{title}", post().to(create::create))
+            .route("get", get().to(get::get)),
+    );
 }
