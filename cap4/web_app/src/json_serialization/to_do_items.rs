50737a41402bd36ab45fc8c0ef0a499f8050eba5
MunrraMT <dev.armsantos@gmail.com>
2024-07-17 22:41:34 -0400

    Packaging our custom serialized struct to be returned to users

--- cap4/web_app/src/json_serialization/to_do_items.rs
+++ cap4/web_app/src/json_serialization/to_do_items.rs
@@ -1,6 +1,10 @@
+use actix_web::{body::BoxBody, http::header::ContentType, HttpResponse, Responder};
 use serde::Serialize;
 
-use crate::to_do::{structs::base::Base, ItemTypes};
+use crate::{
+    state::read_file,
+    to_do::{enums::TaskStatus, structs::base::Base, to_do_factory, ItemTypes},
+};
 
 #[derive(Serialize)]
 pub struct ToDoItems {
@@ -32,5 +36,31 @@ impl ToDoItems {
             pending_item_count: pending_count,
             done_item_count: done_count,
         }
+    }
+
+    pub fn get_state() -> Self {
+        let state = read_file("./state.json");
+        let mut array_buffer = Vec::new();
+
+        for (key, value) in state {
+            let status = TaskStatus::from_string(&value.as_str().unwrap().to_string());
+            let item = to_do_factory(&key, status);
+
+            array_buffer.push(item);
+        }
+
+        Self::new(array_buffer)
+    }
+}
+
+impl Responder for ToDoItems {
+    type Body = BoxBody;
+
+    fn respond_to(self, req: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
     }
 }
