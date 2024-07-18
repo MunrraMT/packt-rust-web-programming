9e6a24868adae9f75a6d905f093619590b191ae4
MunrraMT <dev.armsantos@gmail.com>
2024-07-17 23:39:53 -0400

    Extracting data from the header in requests

--- cap4/web_app/src/main.rs
+++ cap4/web_app/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+use actix_service::Service;
 use actix_web::{App, HttpServer};
 
 mod json_serialization;
@@ -11,7 +12,17 @@ mod views;
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     HttpServer::new(|| {
-        let app = App::new().configure(views::views_factory);
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                println!("{:?}", req);
+                let future = srv.call(req);
+                async {
+                    let result = future.await?;
+
+                    Ok(result)
+                }
+            })
+            .configure(views::views_factory);
         return app;
     })
     .bind("0.0.0.0:8080")?
