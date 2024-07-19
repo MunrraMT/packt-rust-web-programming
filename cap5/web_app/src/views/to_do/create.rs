972dd7f46de3805a2bf8e8c67bc858a04f3a8731
MunrraMT <dev.armsantos@gmail.com>
2024-07-19 00:42:02 -0400

    Communicating with our server using JavaScript

--- cap5/web_app/src/views/to_do/create.rs
+++ cap5/web_app/src/views/to_do/create.rs
@@ -1,17 +1,18 @@
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, HttpResponse};
 
 use crate::{
+    json_serialization::to_do_items::ToDoItems,
     process::process_input,
     state::read_file,
     to_do::{enums::TaskStatus, to_do_factory},
 };
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state = read_file("./state.json");
     let title = req.match_info().get("title").unwrap().to_string();
     let item = to_do_factory(&title.as_str(), TaskStatus::PENDING);
 
     process_input(item, "create".to_string(), &state);
 
-    format!("{} created", title)
+    return HttpResponse::Ok().json(ToDoItems::get_state());
 }
