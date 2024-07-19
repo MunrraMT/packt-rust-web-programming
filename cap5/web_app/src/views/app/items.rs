52621180c972549d83215aef15d278517f0c8132
MunrraMT <dev.armsantos@gmail.com>
2024-07-19 01:07:33 -0400

    Adding JavaScript tags in the HTML

--- cap5/web_app/src/views/app/items.rs
+++ cap5/web_app/src/views/app/items.rs
@@ -4,7 +4,7 @@ use super::content_loader::read_file;
 
 pub async fn items() -> HttpResponse {
     let js_data = read_file("./js/main.js");
-    let html_data = read_file("./templates/main.html").replace("{{JAVASCRIPT}}", &js_data);
+    let html_data = read_file("./templates/main.html").replace("// {{JAVASCRIPT}}", &js_data);
 
     return HttpResponse::Ok()
         .content_type(ContentType::html())
