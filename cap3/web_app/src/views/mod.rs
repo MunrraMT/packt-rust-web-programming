42442b8c8623f8f4756dc6e7587c4fa1d5ebb7bd
MunrraMT <dev.armsantos@gmail.com>
2024-09-24 02:09:01 -0400

    reset to cap3

--- cap3/web_app/src/views/mod.rs
+++
@@ -1,8 +0,0 @@
-use actix_web::web::ServiceConfig;
-use auth::auth_view_factory;
-
-mod auth;
-
-pub fn views_factory(app: &mut ServiceConfig) {
-    auth_view_factory(app);
-}
--- cap5/web_app/src/to_do/structs/base.rs
+++
@@ -1,9 +0,0 @@
-use serde::Serialize;
-
-use super::super::enums::TaskStatus;
-
-#[derive(Serialize)]
-pub struct Base {
-    pub title: String,
-    pub status: TaskStatus,
-}
