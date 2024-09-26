005f696e06c7ed4135ca3585617249dd5da70cdb
MunrraMT <dev.armsantos@gmail.com>
2024-09-25 20:28:10 -0400

    integrating serialization structs into our application code

--- cap4/web_app/src/json_serialization/to_do_items.rs
+++ cap4/web_app/src/json_serialization/to_do_items.rs
@@ -7,3 +7,27 @@ pub struct ToDoItems {
     pub pending_item_count: u8,
     pub done_item_count: u8,
 }
+
+impl ToDoItems {
+    pub fn new(input_items: Vec<to_do::ItemTypes>) -> ToDoItems {
+        let mut pending_array_buffer = Vec::new();
+        let mut done_array_buffer = Vec::new();
+
+        for item in input_items {
+            match item {
+                to_do::ItemTypes::Pending(packed) => pending_array_buffer.push(packed.super_struct),
+                to_do::ItemTypes::Done(packed) => done_array_buffer.push(packed.super_struct),
+            }
+        }
+
+        let done_count = done_array_buffer.len() as u8;
+        let pending_count = pending_array_buffer.len() as u8;
+
+        return Self {
+            pending_items: pending_array_buffer,
+            done_items: done_array_buffer,
+            pending_item_count: pending_count,
+            done_item_count: done_count,
+        };
+    }
+}
--- cap4/web_app/src/views/to_do/get.rs
+++ cap4/web_app/src/views/to_do/get.rs
@@ -1,6 +1,16 @@
-use crate::state;
+use crate::{json_serialization, state, to_do};
 
 pub async fn get() -> impl actix_web::Responder {
     let current_state = state::read_file("./state.json");
-    return actix_web::web::Json(current_state);
+    let mut array_buffer = Vec::new();
+
+    for (key, value) in &current_state {
+        let current_status =
+            to_do::enums::TaskStatus::from_string(value.as_str().unwrap().to_string());
+        let item = to_do::to_do_factory(&key, current_status);
+        array_buffer.push(item);
+    }
+
+    let return_package = json_serialization::to_do_items::ToDoItems::new(array_buffer);
+    return actix_web::web::Json(return_package);
 }
