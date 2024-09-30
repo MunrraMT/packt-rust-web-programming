59d2d3576a4aa71deef89dc575de29f73e00a28e
MunrraMT <dev.armsantos@gmail.com>
2024-09-30 14:32:49 -0400

    Communicating with our server using JavaScript

--- cap5/web_app/src/views/to_do/create.rs
+++ cap5/web_app/src/views/to_do/create.rs
@@ -1,21 +1,22 @@
-use crate::{jwt, process, state, to_do};
+use crate::{json_serialization, jwt, process, state, to_do};
 
-pub async fn create(req: actix_web::HttpRequest, token: jwt::JwtToken) -> String {
+pub async fn create(req: actix_web::HttpRequest, token: jwt::JwtToken) -> actix_web::HttpResponse {
     dbg!(&token);
     println!("here is the message in the token: {}", &token.message);
 
     let current_state = state::read_file("./state.json");
 
-    match req.match_info().get("title") {
-        Some(title_valid) => {
-            let item = to_do::to_do_factory(&title_valid, to_do::enums::TaskStatus::PENDING);
-
-            process::process_input(item, "create".to_string(), &current_state);
-
-            return format!("{title_valid} created");
-        }
+    let title = match req.match_info().get("title") {
+        Some(title_valid) => title_valid,
         None => {
-            return "title not found".to_string();
+            return actix_web::HttpResponse::BadRequest().json("title not found".to_string());
         }
-    }
+    };
+
+    let item = to_do::to_do_factory(&title, to_do::enums::TaskStatus::PENDING);
+
+    process::process_input(item, "create".to_string(), &current_state);
+
+    return actix_web::HttpResponse::Ok()
+        .json(json_serialization::to_do_items::ToDoItems::get_state());
 }
