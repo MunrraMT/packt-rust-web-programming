bfe45bd317db714b2a7fcdcaa340ddf458a72cfd
MunrraMT <dev.armsantos@gmail.com>
2024-09-30 16:13:16 -0400

    Injecting CSS into HTML

--- cap5/web_app/src/views/app/items.rs
+++ cap5/web_app/src/views/app/items.rs
@@ -3,7 +3,9 @@ use super::content_loader;
 pub async fn items() -> actix_web::HttpResponse {
     let javascript_data = content_loader::read_file("src/javascript/main.js");
     let html_data = content_loader::read_file("src/templates/main.html")
-        .replace("{ { JAVASCRIPT } }", &javascript_data);
+        .replace("//{{JAVASCRIPT}}", &javascript_data)
+        .replace("/*{{BASE_CSS}}*/", "")
+        .replace("/*{{CSS}}*/", "");
 
     return actix_web::HttpResponse::Ok()
         .content_type(actix_web::http::header::ContentType::html())
