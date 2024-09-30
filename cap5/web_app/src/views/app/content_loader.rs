1a2edcc0401a13b15e6fc1ce4475a2c36b57d007
MunrraMT <dev.armsantos@gmail.com>
2024-09-30 14:23:08 -0400

    Serving basic HTML loaded from files

--- cap5/web_app/src/views/app/content_loader.rs
+++ cap5/web_app/src/views/app/content_loader.rs
@@ -1,4 +1,18 @@
 pub fn read_file(file_path: &str) -> String {
-    let data = std::fs::read_to_string(file_path).unwrap_or("".to_string());
+    let data = std::fs::read_to_string(file_path).unwrap_or_else(|error| {
+        dbg!(error);
+        let html_page_not_found = read_file_page_not_found();
+        return html_page_not_found;
+    });
+
+    return data;
+}
+
+pub fn read_file_page_not_found() -> String {
+    let data = std::fs::read_to_string("src/templates/404.html").unwrap_or_else(|error| {
+        dbg!(error);
+        return "Page not found".to_string();
+    });
+
     return data;
 }
--- cap5/web_app/src/views/app/items.rs
+++ cap5/web_app/src/views/app/items.rs
@@ -1,5 +1,8 @@
+use super::content_loader;
+
 pub async fn items() -> actix_web::HttpResponse {
+    let html_data = content_loader::read_file("src/templates/main.html");
     return actix_web::HttpResponse::Ok()
         .content_type(actix_web::http::header::ContentType::html())
-        .body("<h1>Items</h1>");
+        .body(html_data);
 }
