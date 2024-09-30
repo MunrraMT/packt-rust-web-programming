9f215fda1a30d1cbd48ee7456a5d1c63372526cd
MunrraMT <dev.armsantos@gmail.com>
2024-09-30 15:49:47 -0400

    Adding JavaScript tags in the HTML

--- cap5/web_app/src/views/app/items.rs
+++ cap5/web_app/src/views/app/items.rs
@@ -3,7 +3,7 @@ use super::content_loader;
 pub async fn items() -> actix_web::HttpResponse {
     let javascript_data = content_loader::read_file("src/javascript/main.js");
     let html_data = content_loader::read_file("src/templates/main.html")
-        .replace("{ { javascript } }", &javascript_data);
+        .replace("{ { JAVASCRIPT } }", &javascript_data);
 
     return actix_web::HttpResponse::Ok()
         .content_type(actix_web::http::header::ContentType::html())
