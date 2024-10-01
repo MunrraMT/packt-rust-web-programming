8703f39d0c51f25b099ff7b137d4c561ca18ac7d
MunrraMT <dev.armsantos@gmail.com>
2024-10-01 01:14:15 -0400

    Building a database connection pool

--- cap6/web_app/src/database.rs
+++ cap6/web_app/src/database.rs
@@ -1,10 +1,60 @@
-use diesel::Connection;
+use actix_web::dev::Payload;
+use actix_web::error::ErrorServiceUnavailable;
+use actix_web::{Error, FromRequest, HttpRequest};
+use futures::future::{err, ok, Ready};
 
-pub fn establish_connection() -> diesel::pg::PgConnection {
-    dotenv::dotenv().ok();
+use crate::config::Config;
+use diesel::{
+    pg::PgConnection,
+    r2d2::{ConnectionManager, Pool, PooledConnection},
+};
+use lazy_static::lazy_static;
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+type PgPool = Pool<ConnectionManager<PgConnection>>;
 
-    return diesel::pg::PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connection to {}", database_url));
+pub struct DbConnection {
+    pub db_connection: PgPool,
+}
+
+lazy_static! {
+    pub static ref DBCONNECTION: DbConnection = {
+        let connection_string = Config::new()
+            .map
+            .get("DB_URL")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_string();
+
+        return DbConnection {
+            db_connection: PgPool::builder()
+                .max_size(8)
+                .build(ConnectionManager::new(connection_string))
+                .expect("failed to create db connection_pool"),
+        };
+    };
+}
+
+pub fn establish_connection() -> PooledConnection<ConnectionManager<PgConnection>> {
+    return DBCONNECTION.db_connection.get().unwrap();
+}
+
+pub struct DB {
+    pub connection: PooledConnection<ConnectionManager<PgConnection>>,
+}
+
+impl FromRequest for DB {
+    type Error = Error;
+    type Future = Ready<Result<DB, Error>>;
+
+    fn from_request(_: &HttpRequest, _: &mut Payload) -> Self::Future {
+        match DBCONNECTION.db_connection.get() {
+            Ok(connection) => return ok(DB { connection }),
+            Err(_) => {
+                return err(ErrorServiceUnavailable(
+                    "could not make connection to database",
+                ))
+            }
+        }
+    }
 }
