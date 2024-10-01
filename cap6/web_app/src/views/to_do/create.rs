c6326ec6b2cc9af9ef601d29334a3ecea45af198
MunrraMT <dev.armsantos@gmail.com>
2024-10-01 00:20:42 -0400

    Inserting into the database

--- cap6/web_app/src/views/to_do/create.rs
+++ cap6/web_app/src/views/to_do/create.rs
@@ -1,21 +1,31 @@
-use crate::{json_serialization, jwt, process, state, to_do};
+use crate::{diesel, models};
+use diesel::prelude::*;
+
+use crate::{database, json_serialization, jwt, schema};
 
 pub async fn create(req: actix_web::HttpRequest, token: jwt::JwtToken) -> actix_web::HttpResponse {
     dbg!(&token);
     println!("here is the message in the token: {}", &token.message);
 
-    let current_state = state::read_file("./state.json");
-
     let title = match req.match_info().get("title") {
-        Some(title_valid) => title_valid,
+        Some(title_valid) => title_valid.to_string(),
         None => {
             return actix_web::HttpResponse::BadRequest().json("title not found".to_string());
         }
     };
+    let connection = &mut database::establish_connection();
+    let items = schema::to_do::table
+        .filter(schema::to_do::columns::title.eq(&title.as_str()))
+        .order(schema::to_do::columns::id.asc())
+        .load::<models::item::item::Item>(connection)
+        .unwrap();
 
-    let item = to_do::to_do_factory(&title, to_do::enums::TaskStatus::PENDING);
-
-    process::process_input(item, "create".to_string(), &current_state);
+    if items.len() == 0 {
+        let new_post = models::item::new_item::NewItem::new(title);
+        let _ = diesel::insert_into(schema::to_do::table)
+            .values(&new_post)
+            .execute(connection);
+    }
 
     return actix_web::HttpResponse::Ok()
         .json(json_serialization::to_do_items::ToDoItems::get_state());
